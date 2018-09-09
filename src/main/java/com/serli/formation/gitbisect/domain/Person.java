231ca24194d9f27b0f4ea54df0108891d3139175
Gauthier POULET <gauthier.poulet@gmail.com>
2018-09-09 19:04:36 +0200

    add Person pojo

---
+++ src/main/java/com/serli/formation/gitbisect/domain/Person.java
@@ -0,0 +1,13 @@
+package com.serli.formation.gitbisect;
+
+public class Person {
+
+    private String surname;
+
+    private String lastname;
+
+    public Person(String surname, String lastname) {
+        this.surname = surname;
+        this.lastname = lastname;
+    }
+}
