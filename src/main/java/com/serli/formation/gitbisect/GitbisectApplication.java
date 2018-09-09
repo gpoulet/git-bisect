1ec75f50ca774a4beae3719a5eb67bc06e19a93f
Gauthier POULET <gauthier.poulet@gmail.com>
2018-09-09 19:02:22 +0200

    initial commit

---
+++ src/main/java/com/serli/formation/gitbisect/GitbisectApplication.java
@@ -0,0 +1,12 @@
+package com.serli.formation.gitbisect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbisectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbisectApplication.class, args);
+	}
+}
